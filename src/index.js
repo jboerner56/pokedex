771c8b280e3c085e19c3c5251e9c64bee937673b
Jason Boerner <jboerner56@gamil.com>
2019-05-07 09:49:59 -0400

    add skeleton for redux code w/ pokemon deck

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import initialState from './base.json'
+console.log(initialState);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +12,40 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//state is an objexct with a cards property which is an array of objects
+// {cards: [{}, {}, {}, ]}
+
+
+//State
+
+
+// ===============================================
+// Actions + Action creators
+const ACTION_CATCH = 'catch';
+
+function catchCard(id) {
+    return {
+        type: ACTION_CATCH,
+        payload: {
+            id,
+            //id: id,  same as above, key and value are the same so can ommit colon and value 
+
+        }
+    }
+}
+// ===============================================
+// Reducer
+function cards(state=initialState, action={type: ''}) {
+    switch(action.type){
+        case ACTION_CATCH:
+        // find card, and set it to caught
+
+        break;
+        default:
+            return state;
+        break;
+    }
+
+}
+// ===============================================
+// Store
