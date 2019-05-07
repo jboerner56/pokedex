f34bea1504177c76cb1a5214a4bdc69d14065c63
Jason Boerner <jboerner56@gamil.com>
2019-05-07 13:23:24 -0400

    add provider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import initialCards from './base.json'
 import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 // console.log(initialState);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
